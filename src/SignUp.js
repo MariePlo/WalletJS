6c334abe575fef202aa8df95dd14b012328db9a2
Marie Ploteau <marieploteau22@gmail.com>
2019-10-23 18:31:30 +0200

    second commit

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,31 +1,24 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import {Link, Switch, BrowserRouter as Router, Route} from 'react-router-dom';
 import logo from './logoWat.png';
 import './App.css';
+import App from './App.js';
 import Login from './Login.js';
-import SignUp from './SignUp.js';
 
 
-class App extends Component
+class SignUp extends Component
 {
   render()
   {
     return(
-      <Router>
       <div className="App">
       <header className="App-header">
       <img src={logo} className="App-logo" alt="logo"/>
-      <h1 className="App-title">Welcome !</h1>
       <Login className="App-form"/>
-      <Link to={"/SignUp"}>Sign up</Link>
-      <Switch>
-        <Route path='/SignUp' component={SignUp}/>
-        
-      </Switch>
+      <Link to={'/App'}>Home</Link>
       </header>
       </div>
-      </Router>
       );
   }
 }
- export default App;
+ export default SignUp;
