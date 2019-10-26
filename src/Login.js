259bdbfd70500afe6a36bfcf3fc13e6350e0f197
Marie Ploteau <marieploteau22@gmail.com>
2019-10-26 17:32:06 +0200

    LOGIN WORKS  gej,kdrizeboerhvrjfh!!!!

--- src/Login.js
+++ src/Login.js
@@ -3,18 +3,32 @@ import React, { Component } from 'react';
 class Login extends Component 
 {
 
-	validate = (mail, password) =>
+	handleChange(event)
 	{
-		
+		if(localStorage.getItem([event.target.name])===event.target.value)
+		{
+			var i = sessionStorage.getItem("test");
+			sessionStorage.removeItem("test");
+			i++;
+			sessionStorage.setItem("test", i);
+		}
+
+		if(sessionStorage.getItem("test")==='2')
+		{
+			sessionStorage.removeItem("state");
+			sessionStorage.setItem("state", '1');
+		}
+
 	}
 
 
 	render()
 	{
+		sessionStorage.setItem("test", '0');
 		return(
 			<form>
-				<label className="element-form">Email Address : <input type="mail" id="in_mail" required/></label>
-				<label className="element-form">Password : <input type="password" id="in_password" required/></label>
+				<label className="element-form">Email Address : <input type="mail" name="mail" onChange={this.handleChange} required/></label>
+				<label className="element-form">Password : <input type="password" name="password" onChange={this.handleChange} required/></label>
 				<button type="submit">Login</button>
 			</form>
 		);
